6a04da8d3db423e58c6c7794aa360f3480ae985b
KISHAN1506 <kishanagarwal1506@gmail.com>
2025-03-01 13:18:56 +0530

    Abstract Class

---
+++ 09Code.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+/*
+Abstraction : Abstraction is hiding all the unnecessary (or sensetive) details and showing only
+the important parts
+
+Access Modifier / Specifier are example of Abstraction
+
+*/
+
+
+/*
+Abstract Classes:
+
+1. Abstract classes are used to provide a base class from which other classes can be derived.
+2. They cannot be instantiated(their object cannot be made) and are meant to be inherited.
+3. Abstract classes are typically used to define an interface for derived classes.
+*/
+
+
+class Shape{ // abstract class
+    virtual void draw() = 0; // pure virtual function
+};
+
+class Circle : public Shape{
+    public:
+    void draw(){
+        cout<<"Drawing a circle";
+    }
+};
+
+int main(){
+    Circle c1;
+    c1.draw();
+    return 0;
+}
